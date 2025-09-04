a20a1ad6d52d3795ae4675ccb092902eb60626f2
Prokop Hanzl <prokophanzl666@gmail.com>
2025-09-05 00:36:59 +0200

    Continue work

--- assets/js/display_friends.js
+++ assets/js/display_friends.js
@@ -131,6 +131,7 @@ function open_sort_menu() {
 	const menu = document.createElement("div");
 	menu.id = "sort-menu";
 	menu.innerHTML = `
+		<div class="back-icon"></div>
 		<div class="menu-title">Sort Friends</div>
 		<div class="option" data-sort="longest-overdue">
 			<div class="radio ${currentSort === "longest-overdue" ? "active" : ""}"></div>
@@ -176,6 +177,48 @@ function open_sort_menu() {
 			menu.remove();
 		});
 	});
+
+	// add click listener for back-icon to just close menu
+	const backIcon = menu.querySelector(".back-icon");
+	if (backIcon) {
+		backIcon.addEventListener("click", () => {
+			menu.remove();
+		});
+	}
+}
+
+function open_account_menu() {
+	// remove if menu already exists
+	const existingMenu = document.getElementById("account-menu");
+	if (existingMenu) existingMenu.remove();
+
+	// create menu container
+	const menu = document.createElement("div");
+	menu.id = "account-menu";
+	menu.innerHTML = `
+		<div class="back-icon"></div>
+		<div class="menu-title">
+			<div class="menu-name">Ash Hanžl</div>
+			<div class="menu-subtitle">ashhanzl03@gmail.com</div>
+		</div>
+		<div class="option" onclick="window.location.href = 'change_name.html'">
+			<div class="option-label">Change Name</div>
+		</div>
+		<div class="option" id="sign-out-option">
+			<div class="option-label">Sign Out</div>
+		</div>
+	`;
+
+	// append to body
+	document.body.appendChild(menu);
+
+	// back-icon closes menu
+	const backIcon = menu.querySelector(".back-icon");
+	if (backIcon) {
+		backIcon.addEventListener("click", () => {
+			menu.remove();
+		});
+	}
 }
 
 let currentSort = localStorage.getItem("preferredSort") || "longest-overdue";
